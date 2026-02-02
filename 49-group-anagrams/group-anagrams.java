4d3a1fd3fe007b2749fef94c838c7bb7b59fe623
saisnehith876 <saisnehith876@gmail.com>
2026-02-02 21:36:24 +0530

    Time: 20 ms (16.46%) | Memory: 50.6 MB (15.28%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String,List> map = new HashMap<>();
+
+        if(strs.length ==0){
+            return new ArrayList();
+        }
+
+        int[] count = new int[26];
+
+        for(String s: strs){
+            Arrays.fill(count,0);
+            for(char c:s.toCharArray()){
+                count[c-'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder("");
+
+            for(int i=0;i<26;i++){
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key =sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList());
+                
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+
+        
+    }
+}

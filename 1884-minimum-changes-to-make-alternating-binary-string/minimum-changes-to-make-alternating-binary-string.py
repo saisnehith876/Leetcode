c43eacea3c2f6d2997ab153b543ae01faa2c05af
saisnehith876 <saisnehith876@gmail.com>
2026-03-06 07:02:09 +0530

    Time: 7 ms (83.04%) | Memory: 19.3 MB (56.02%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,7 +1,13 @@
 class Solution:
     def minOperations(self, s: str) -> int:
+        diff = 0
 
-        l = 0
+        for i in range(len(s)):
+            if s[i] != ("1" if i % 2 == 0 else '0'):
+                diff += 1
+
+        return min(diff, len(s) - diff)
+        '''l = 0
         r = 0
 
         for i in range(len(s)):
@@ -10,4 +16,4 @@ class Solution:
             if s[i] != str((i + 1) % 2):
                 r += 1
 
-        return min(l, r)
+        return min(l, r)'''

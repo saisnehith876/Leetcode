d6aec4b8e1a9532a87c449645d02f753a4a55f69
Snehith876 <saisnehith876@gmail.com>
2026-06-15 09:01:23 +0530

    Time: 9 ms (30.63%) | Memory: 238 MB (68.59%) - LeetSync

---
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
+
+        int bestLandFinish = INT_MAX, bestWaterFinish = INT_MAX;
+        for (int i = 0; i < n; i++)
+            bestLandFinish = min(bestLandFinish, landStartTime[i] + landDuration[i]);
+        for (int j = 0; j < m; j++)
+            bestWaterFinish = min(bestWaterFinish, waterStartTime[j] + waterDuration[j]);
+
+        int ans = INT_MAX;
+
+        // land first then water
+        for (int j = 0; j < m; j++)
+            ans = min(ans, max(bestLandFinish, waterStartTime[j]) + waterDuration[j]);
+
+        // water first then land
+        for (int i = 0; i < n; i++)
+            ans = min(ans, max(bestWaterFinish, landStartTime[i]) + landDuration[i]);
+
+        return ans;
+        
+    }
+};
